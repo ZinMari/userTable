a7c95b6eeef34157139715031121cee2ee53054e
Mari Zin <mari.v.zinkevich@gmail.com>
2025-08-15 13:48:49 +0300

    feat: add error handling to useFetch

--- src/hooks/useFetch.tsx
+++ src/hooks/useFetch.tsx
@@ -3,16 +3,19 @@ import { useEffect, useState } from "react";
 export function useFetch(url: string) {
   const [data, setData] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     setIsLoading(true);
     setData(null);
+    setError(null);
 
     fetch(url)
       .then((res) => res.json())
       .then((respData) => setData(respData))
+      .catch((e) => setError(e))
       .finally(() => setIsLoading(false));
   }, [url]);
 
-  return [data, isLoading];
+  return [data, isLoading, error];
 }
